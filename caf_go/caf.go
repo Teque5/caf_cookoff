c415341b3eb1de450026bf06bd4977db3716672c
Teque5 <teque5@gmail.com>
2020-02-15 20:53:13 -0800

    updated to use complex64 samples from generate.py

--- caf_go/caf.go
+++ caf_go/caf.go
@@ -14,6 +14,27 @@ import (
 	"github.com/mjibson/go-dsp/fft"
 )
 
+func load_c64(path string) (ray []complex64, err error) {
+  handle, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open handle %v", err)
+	}
+	defer handle.Close()
+	info, err := handle.Stat()
+	if err != nil {
+		return nil, err
+	}
+	sample_count := info.Size() / 8
+	fmt.Println(sample_count)
+	ray = make([]complex64, sample_count)
+
+	err = binary.Read(handle, binary.LittleEndian, &ray)
+	if err != nil {
+		log.Fatalf("error reading floats %v", err)
+	}
+	return ray, nil
+}
+
 func load_f32(path string) (ray []float32, err error) {
 	handle, err := os.Open(path)
 	if err != nil {
@@ -45,11 +66,24 @@ func f32_to_c128(ray_i []float32) (ray_iq []complex128) {
 	return ray_iq
 }
 
+func c64_to_c128(ray_i []complex64) (ray_iq []complex128) {
+	// Convert real float32 to complex64
+	len_ray := len(ray_i)
+	ray_iq = make([]complex128, len_ray)
+	for idx := 0; idx < len_ray; idx += 1 {
+		ray_iq[idx] = complex128(ray_i[idx])
+	}
+	return ray_iq
+}
+
 func xcor(apple []complex128, banana []complex128) (corr []complex128) {
 	/*
 	  Standard crosscorrelation implementation
 	*/
 	len_ray := len(apple)
+  if len(banana) != len_ray {
+    panic("input arrays should be same size")
+  }
 	apple_fft := fft.FFT(apple)
 	banana_fft := fft.FFT(banana)
 	fleeb := make([]complex128, len_ray)
@@ -70,24 +104,26 @@ func apply_fdoa(ray []complex128, fdoa float64, samp_rate float64) []complex128 
   return ray
 }
 
+// func amb_surf(needle []complex128, haystack []complex128, freqs_hz []float64, samp_rate float64) (surf [][]complex128) {
+//
+// }
+
 func main() {
 	data_path := "../data/"
-	apple, err := load_f32(data_path + "burst_0000_raw.f32")
+	apple, err := load_c64(data_path + "chirp_0_raw.c64")
 	if err != nil {
 		log.Fatal(err)
 	}
-	apple_iq := f32_to_c128(apple)
-	banana, err := load_f32(data_path + "burst_0000_t+20.007860_f+88.202617.f32")
+	apple_iq := c64_to_c128(apple)
+	banana, err := load_c64(data_path + "chirp_0_T+202samp_F.+69.25Hz.c64")
 	if err != nil {
 		log.Fatal(err)
 	}
-	banana_iq := f32_to_c128(banana)
+	banana_iq := c64_to_c128(banana)[0:4096]
 	corr := xcor(apple_iq, banana_iq)
   trash := []complex128{1,2,3,4,5,6,7,8,9,10}
 	fmt.Println(trash)
   fmt.Println(len(corr))
-  z := apply_fdoa(trash, 200, 48e3)
+  z := apply_fdoa(corr, 200, 48e3)
   fmt.Println(z)
-
-
 }
