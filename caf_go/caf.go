95f0d633d9d0a8e74954c8556b158ad8b32182a4
Teque5 <teque5@gmail.com>
2020-02-15 22:03:01 -0800

    code cleanup; something still broken

--- caf_go/caf.go
+++ caf_go/caf.go
@@ -6,16 +6,15 @@ import (
 	"log"
 	"math/cmplx"
 	"os"
-	// "sort"
-	"math"
 	"encoding/binary"
+	"math"
 	// "log"
-	// "gopkg.in/yaml.v2"
 	"github.com/mjibson/go-dsp/fft"
 )
 
 func load_c64(path string) (ray []complex64, err error) {
-  handle, err := os.Open(path)
+	// Load complex file
+	handle, err := os.Open(path)
 	if err != nil {
 		return nil, fmt.Errorf("cannot open handle %v", err)
 	}
@@ -36,6 +35,7 @@ func load_c64(path string) (ray []complex64, err error) {
 }
 
 func load_f32(path string) (ray []float32, err error) {
+	// Load float file
 	handle, err := os.Open(path)
 	if err != nil {
 		return nil, fmt.Errorf("cannot open handle %v", err)
@@ -57,7 +57,7 @@ func load_f32(path string) (ray []float32, err error) {
 }
 
 func f32_to_c128(ray_i []float32) (ray_iq []complex128) {
-	// Convert real float32 to complex64
+	// Convert real float32 to complex128
 	len_ray := len(ray_i)
 	ray_iq = make([]complex128, len_ray)
 	for idx := 0; idx < len_ray; idx += 1 {
@@ -67,7 +67,7 @@ func f32_to_c128(ray_i []float32) (ray_iq []complex128) {
 }
 
 func c64_to_c128(ray_i []complex64) (ray_iq []complex128) {
-	// Convert real float32 to complex64
+	// Convert real complex64 to complex128
 	len_ray := len(ray_i)
 	ray_iq = make([]complex128, len_ray)
 	for idx := 0; idx < len_ray; idx += 1 {
@@ -77,13 +77,11 @@ func c64_to_c128(ray_i []complex64) (ray_iq []complex128) {
 }
 
 func xcor(apple []complex128, banana []complex128) (corr_abs []float64) {
-	/*
-	  Standard crosscorrelation implementation
-	*/
+	// Standard crosscorrelation implementation
 	len_ray := len(apple)
-  if len(banana) != len_ray {
-    panic("input arrays should be same size")
-  }
+	if len(banana) != len_ray {
+		panic("input arrays should be same size")
+	}
 	apple_fft := fft.FFT(apple)
 	banana_fft := fft.FFT(banana)
 	fleeb := make([]complex128, len_ray)
@@ -95,28 +93,25 @@ func xcor(apple []complex128, banana []complex128) (corr_abs []float64) {
 	corr_abs = make([]float64, len_ray)
 	corr = fft.IFFT(fleeb)
 
-	for idx, val := range(corr) {
+	for idx, val := range corr {
 		corr_abs[idx] = cmplx.Abs(val)
 	}
 	return
 }
 
 func apply_fdoa(ray []complex128, fdoa float64, samp_rate float64) []complex128 {
-  len_ray := len(ray)
-  pre := complex(0, -2 * math.Pi * fdoa / samp_rate)
-  // fmt.Println(pre)
-  for idx := 0; idx <  len_ray; idx += 1 {
-    ray[idx] = ray[idx] * cmplx.Exp(pre * complex(float64(idx), 0))
-  }
-  return ray
+	len_ray := len(ray)
+	precache := complex(0, -2*math.Pi*fdoa/samp_rate)
+	for idx := 0; idx < len_ray; idx += 1 {
+		ray[idx] = ray[idx] * cmplx.Exp(precache*complex(float64(idx), 0))
+	}
+	return ray
 }
 
 func amb_surf(needle []complex128, haystack []complex128, freqs_hz []float64, samp_rate float64) (surf [][]float64) {
-	time_bins := len(needle)
-	// freq_bins := len(freqs_hz)
-	// surf = [time_bins][freq_bins]floatmake([][]float64, time_bins, freq_bins)
-	shifted := make([]complex128, time_bins)
-	corr := make([]float64, time_bins)
+	len_ray := len(needle)
+	shifted := make([]complex128, len_ray)
+	corr := make([]float64, len_ray)
 	for _, freq_hz := range freqs_hz {
 		shifted = apply_fdoa(needle, freq_hz, samp_rate)
 		corr = xcor(shifted, haystack)
@@ -126,14 +121,30 @@ func amb_surf(needle []complex128, haystack []complex128, freqs_hz []float64, sa
 }
 
 func arange(start, stop, step float64) (rnge []float64) {
-    N := int(math.Ceil((stop - start) / step));
-    rnge = make([]float64, N, N)
-    i := 0
-    for x := start; x < stop; x += step {
-        rnge[i] = x;
-        i += 1
-    }
-    return
+	N := int(math.Ceil((stop - start) / step))
+	rnge = make([]float64, N, N)
+	i := 0
+	for x := start; x < stop; x += step {
+		rnge[i] = x
+		i += 1
+	}
+	return
+}
+
+func find_2d_peak(surf [][]float64) (fmax int, tmax int) {
+	// 2D Argmax
+	var max float64 = 0
+	for rdx, row := range surf {
+		for vdx, elem := range row {
+			if elem > max {
+				max = elem
+				fmax = rdx
+				tmax = vdx
+				fmt.Println("up", fmax, tmax)
+			}
+		}
+	}
+	return
 }
 
 func main() {
@@ -148,11 +159,10 @@ func main() {
 		log.Fatal(err)
 	}
 	banana_iq := c64_to_c128(banana)[0:4096]
-	// corr := xcor(apple_iq, banana_iq)
 
 	freqs_hz := arange(-100, 100, 0.5)
-	// fmt.Println(freqs_hz)
 	surf := amb_surf(apple_iq, banana_iq, freqs_hz, 48000)
-	fmt.Println(len(surf))
+	fdx, tdx := find_2d_peak(surf)
+	fmt.Println(4096-tdx, freqs_hz[fdx])
 
 }
