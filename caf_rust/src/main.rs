7dc07d2197c943607feb616fdda520e4199c3d46
Mark Kubiak <mkubiak.dev@gmail.com>
2020-02-15 19:37:55 -0800

    Confucius says: don't tell me to implement the wrong function

--- caf_rust/src/main.rs
+++ caf_rust/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::prelude::*;
+use std::f32::consts::PI;
 use std::fs::File;
 
 use fftw::array::AlignedVec;
@@ -52,7 +53,6 @@ fn to_complex_32(in_slice: &[f32]) -> Vec<Complex32> {
         samples.push(Complex32::new(*real, 0.0)); // re + 0j
     }
     samples
-
 }
 
 // Cross correlation of 2 complex slices using FFTW
@@ -145,6 +145,27 @@ fn xcor_rustfft(a: &[Complex32], b: &[Complex32]) -> Vec<Complex32> {
     res_time
 }
 
+// Takes in a slice of samples at samp_rate and applies
+// a frequency shift to it
+fn apply_freq_shifts(samples: &mut [Complex32], freq_shift: f32, fs: u32)
+    -> Vec<Complex32> {
+
+    // Convert (back) to vec
+    let mut samples = samples.to_vec();
+
+    // Apply to each sample
+    // x *= e^(-j*2pi*fs*df*t)
+    let dt = 1.0 / (fs as f32);
+    let exp_common = Complex32::new(0.0, -2.0 * PI * dt * freq_shift);
+    for (i, samp) in samples.iter_mut().enumerate() {
+        let exp = Complex32::new(i as f32, 0.0) * exp_common;
+        *samp *= Complex32::exp(&exp);
+    }
+
+    // Return our shifted samples
+    samples 
+}
+
 fn main() {
     let data = to_complex_32(
         &read_file_f32("../data/burst_0000_raw.f32")
@@ -155,6 +176,15 @@ fn main() {
         .unwrap());
 
     let xcor_res = xcor_rustfft(&data, &data_shifted);
+    let mut freq_test = Vec::new();
+    for i in 0..10 {
+        freq_test.push(Complex32::new((i+1) as f32, 0.0));
+    }
+    println!("~~~~~~~~~~");
+    let shifted = apply_freq_shifts(&mut freq_test, 200.0, 48000);
+    for x in shifted {
+        println!("{}", x);
+    }
     // let xcor_res = xcor_fftw(&data, &data_shifted);
     write_file_c32("results.c64", &xcor_res).unwrap();
 }
