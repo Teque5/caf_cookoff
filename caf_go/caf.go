3076a7a82449023e6ee3aa5588e4da0379dad439
Teque5 <teque5@gmail.com>
2020-02-16 21:20:25 -0800

    dramatically simplified arange

--- caf_go/caf.go
+++ caf_go/caf.go
@@ -24,7 +24,7 @@ func dump_surf(path string, surf [][]float64) (err error) {
 			return
 		}
 	}
-	fmt.Println("wrote surf to file", len(surf), len(surf[0]))
+	fmt.Printf("wrote (%vx%v) surf to file\n", len(surf), len(surf[0]))
 	return
 }
 
@@ -146,19 +146,14 @@ func amb_surf(needle []complex128, haystack []complex128, freqs_hz []float64, sa
 }
 
 func arange(start, stop, step float64) (rnge []float64) {
-	N := int(math.Ceil((stop - start) / step))
-	rnge = make([]float64, N, N)
-	i := 0
 	for x := start; x < stop; x += step {
-		rnge[i] = x
-		i += 1
+		rnge = append(rnge, x)
 	}
 	return
 }
 
-func find_2d_peak(ray2d [][]float64) (best_idx int, best_jdx int) {
+func find_2d_peak(ray2d [][]float64) (best_idx int, best_jdx int, max float64) {
 	// 2D Argmax
-	var max float64 = 0
 	for idx, row := range ray2d {
 		for jdx, elem := range row {
 			if elem > max {
@@ -174,26 +169,28 @@ func find_2d_peak(ray2d [][]float64) (best_idx int, best_jdx int) {
 
 func main() {
 
-	start := time.Now()
+
 	data_path := "../data/"
-	apple, err := load_c64(data_path + "chirp_4_raw.c64")
+	apple, err := load_c64(data_path + "chirp_8_raw.c64")
 	if err != nil {
 		log.Fatal(err)
 	}
 	apple_iq := c64_to_c128(apple)
-	banana, err := load_c64(data_path + "chirp_4_T+70samp_F+82.89Hz.c64")
+	banana, err := load_c64(data_path + "chirp_8_T+80samp_F-46.28Hz.c64")
 	if err != nil {
 		log.Fatal(err)
 	}
 	banana_iq := c64_to_c128(banana)[0:4096]
 
-	freqs_hz := arange(-100, 100, 0.5)
+	freqs_hz := arange(-100, 100, .4)
+	start := time.Now()
 	surf := amb_surf(apple_iq, banana_iq, freqs_hz, 48000)
-	fdx, tdx := find_2d_peak(surf)
-	fmt.Println("result:", len(apple_iq)-tdx, "samples", freqs_hz[fdx],"hz")
 	t := time.Now()
 	elapsed := t.Sub(start)
-	fmt.Println(elapsed)
+	fmt.Printf("surf calculated in %s\n", elapsed)
+	fdx, tdx, max := find_2d_peak(surf)
+	fmt.Println("caf result:", len(apple_iq)-tdx, "samples", freqs_hz[fdx],"hz @ amb =",max)
+
 	err = dump_surf("/tmp/derp", surf)
 	if err != nil {
 		log.Fatal(err)
