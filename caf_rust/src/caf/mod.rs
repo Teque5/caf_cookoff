17cb2bb6f3a6a3535145eef5c45dc4e46305cc32
Mark Kubiak <mkubiak.dev@gmail.com>
2020-02-17 03:13:22 -0800

    Update file I/O as trait (great example)

--- caf_rust/src/caf/mod.rs
+++ caf_rust/src/caf/mod.rs
@@ -17,42 +17,62 @@ use xcor_fftw::Xcor;
 // mod xcor_rustfft;
 // use xcor_rustfft::Xcor;
 
+
 // Reads a file of packed 32 bit floats and returns
-// a Vec of its contents
+// a Vec of its contents as Complex64 (2x 64 bit floats)
 pub fn read_file_c64(filename: &str) -> io::Result<Vec<Complex64>> {
+
+    // Open and read a file
     let mut f = File::open(filename)?;
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)?;
     let mut samples = Vec::new();
+
+    // Read each real and imaginary components to a buffer
     for i in (0..buffer.len()).step_by(8) {
+
+        // Real
         let mut real_bytes: [u8; 4] = Default::default();
         real_bytes.copy_from_slice(&buffer[i..i+4]);
         let real = f32::from_le_bytes(real_bytes);
+
+        // Image
         let mut imag_bytes: [u8; 4] = Default::default();
         imag_bytes.copy_from_slice(&buffer[i+4..i+8]);
         let imag = f32::from_le_bytes(imag_bytes);
+
+        // Push to the calling Vec
         samples.push(Complex64::new(real as f64, imag as f64));
     }
     Ok(samples)
 }
 
-// Writes a slice of Complex64s to a file compatible
-// with Numpy's fromfile dtype=np.complex64
-#[allow(dead_code)]
-pub fn write_file_complex(filename: &str, data: &[Complex64]) -> io::Result<()> {
-    let mut f = File::create(filename).unwrap();
-    let mut out_buf: Vec<u8> = Vec::new();
+// Read/write a slice of Complex64's to/from a file
+// compatible with numpy's fromfile function
+pub trait BinaryIO {
+    fn write_file_binary(&self, filename: &str) -> io::Result<()>;
+}
+// numpy dtype=np.complex128
+impl BinaryIO for Vec<Complex64> {
 
-    for bin in data.iter() {
-        for byte in f64::to_le_bytes(bin.re).iter() {
-            out_buf.push(*byte);
+    fn write_file_binary(&self, filename: &str) -> io::Result<()> {
+
+        // Open the file and output byte buffer
+        let mut f = File::create(filename).unwrap();
+        let mut out_buf: Vec<u8> = Vec::new();
+
+        // Write out the real and imag components one-by-one
+        for samp in self.iter() {
+            for byte in f64::to_le_bytes(samp.re).iter() {
+                out_buf.push(*byte);
+            }
+            for byte in f64::to_le_bytes(samp.im).iter() {
+                out_buf.push(*byte);
+            }
         }
-        for byte in f64::to_le_bytes(bin.im).iter() {
-            out_buf.push(*byte);
-        }
+        f.write_all(&out_buf).unwrap();
+        Ok(())
     }
-    f.write_all(&out_buf).unwrap();
-    Ok(())
 }
 
 // Takes in a slice of samples at samp_rate and applies
@@ -231,7 +251,7 @@ mod tests {
         assert_eq!(shifts[freq_idx], -92.75);
         assert_eq!(samp_idx, 177);
     }
-    
+
     #[test]
     fn test_chip6() {
         // Read Chirp 6 reference and modified files
@@ -315,7 +335,7 @@ mod tests {
         // Read in the two files
         let needle = read_file_c64(needle_filename).unwrap();
         let mut haystack = read_file_c64(haystack_filename).unwrap();
-        
+
         // Truncate haystack if necessary
         haystack.resize(needle.len(), Default::default());
 
